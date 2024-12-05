199424900fc67152cba828ae34143950c090b69f
Celso Bitello <98421039+Celso1404@users.noreply.github.com>
2024-12-04 23:56:48 -0300

    Add files via upload

---
+++ SBC_2022/src/Exercicio01/Principal.java
@@ -0,0 +1,16 @@
+package Exercicio01;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Scanner auxiliar = new Scanner(System.in);
+
+        int n;
+        n = auxiliar.nextInt();
+
+        
+
+    }
+}
